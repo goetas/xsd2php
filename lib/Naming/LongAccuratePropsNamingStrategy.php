b0789871074f954c5c3ee0197fd972bdc5061476
Vladimir Belozyorov <VBelozyorov@users.noreply.github.com>
2016-07-21 12:01:35 +0300

    AccuratePropsNamingStrategies

---
+++ lib/Naming/LongAccuratePropsNamingStrategy.php
@@ -0,0 +1,35 @@
+<?php
+namespace Goetas\Xsd\XsdToPhp\Naming;
+
+use Doctrine\Common\Inflector\Inflector;
+use GoetasWebservices\XML\XSDReader\Schema\Item;
+use GoetasWebservices\XML\XSDReader\Schema\Type\Type;
+
+class LongAccuratePropsNamingStrategy implements NamingStrategy
+{
+
+    public function getTypeName(Type $type)
+    {
+        return $this->classify($type->getName()) . "Type";
+    }
+
+    public function getAnonymousTypeName(Type $type, $parentName)
+    {
+        return $this->classify($parentName) . "AnonymousType";
+    }
+
+    public function getItemName(Item $item)
+    {
+        return $this->classify($item->getName());
+    }
+
+    public function getPropertyName($item)
+    {
+        return $item->getName();
+    }
+
+    private function classify($name)
+    {
+        return Inflector::classify(str_replace(".", " ", $name));
+    }
+}
